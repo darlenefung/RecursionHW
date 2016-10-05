86b8a9d91a6f69c6e92b2bfc6ddb7229f7aacf26
Darlene Fung <Darlene@Darlenes-MacBook-Pro.local>
2016-10-05 08:22:59 -0400

    javadoc finished

--- RecursionHW.java
+++ RecursionHW.java
@@ -1,8 +1,6 @@
 /** 
- * This method prints the integers from 
- * 0 to n both inclusive, on separate lines
- * using recursion by 
- * @param n the largest integer printed on the screen
+ * this program prints ints 0 to n
+ * @author Darlene Fung
  */
 public class RecursionHW 
 {
@@ -11,6 +9,13 @@ public class RecursionHW
 		printNum(5);
 	}
 	
+	
+	/** 
+	 * This method prints the integers from 
+ 	 * 0 to n both inclusive, on separate lines
+	 * using recursion by subtracting one from int n
+	 * @param n the largest integer printed on the screen
+	 */	
 	public static void printNum(int n)
 	{
 	if (n > 0)
